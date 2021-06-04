72fcbb5ef9da2899adc0914b7e87d6c7b0d0a542
Chi-kwan Chan <chanc@arizona.edu>
2021-06-04 09:31:25 -0700

    Add viz.show() from blackholepy

---
+++ common/viz.py
@@ -0,0 +1,43 @@
+# Copyright (C) 2020 Chi-kwan Chan
+# Copyright (C) 2020 Steward Observatory
+#
+# This file is part of `blackholepy`.
+#
+# `Blackholepy` is free software: you can redistribute it and/or
+# modify it under the terms of the GNU General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# `Blackholepy` is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with `blackholepy`.  If not, see <http://www.gnu.org/licenses/>.
+
+from matplotlib import pyplot as plt
+
+def show(imgs, s=None, display=False, **kwargs):
+
+    if imgs.ndim != 2:
+        if s is None:
+            raise ValueError('must specify snapshot number for movie')
+        img = imgs[s,:,:]
+    else:
+        img = imgs
+
+    u    = imgs.fov.unit
+    r, t = imgs.fov.value / 2
+
+    switch = not display and plt.isinteractive()
+    if switch:
+        plt.ioff()
+
+    fig, ax = plt.subplots(1, 1)
+    ax.imshow(img.T, origin='lower', extent=[-r, r, -t, t], **kwargs)
+
+    if switch:
+        plt.ion()
+
+    return fig
