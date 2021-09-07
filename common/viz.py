c6e7c9018b3bcdcbb545804cbd7013d19dab3433
Chi-kwan Chan <chanc@arizona.edu>
2021-09-07 07:50:02 -0700

    Add SED plotting tools

--- common/viz.py
+++ common/viz.py
@@ -18,7 +18,8 @@
 
 import numpy as np
 
-from matplotlib import pyplot as plt
+from matplotlib  import pyplot as plt
+from scipy.stats import norm, poisson
 
 
 def show(imgs, s=None, ax=None, **kwargs):
@@ -66,3 +67,56 @@ def ellipse(a0, b0, major, minor, PA, ax=None, diameter=False, **kwargs):
     ax.plot([a[90],a[270]], [b[90],b[270]], '--', linewidth=1, **kwargs)
 
     return ax
+
+
+def interval(avg, std, sigma=1):
+    """We model the different realizations of grmonty results follow a
+    Poisson distribution, which has mean and variance both equal to
+    `mu`.  Therefore,
+
+        avg = dnuLnu * mu
+        std = dnuLnu * sqrt(mu)
+
+        mu = (avg/std)**2
+
+    One we obtain `mu`, we can estimate the lower and upper intervals
+    according to `sigma`.
+
+    """
+    with np.errstate(invalid='ignore', divide='ignore'):
+        mu = (avg/std)**2
+
+    lower = poisson.ppf(norm.cdf(-sigma), mu)
+    upper = poisson.ppf(norm.cdf(+sigma), mu)
+    units = avg / mu
+
+    return lower * units, upper * units
+
+def step_one(ax, nu, avg, std=None, sigma=1,
+             shade=True, ylog=True, **kwargs):
+    p = ax.step(nu, avg, where='mid', **kwargs)
+
+    if std is not None and shade:
+        l, u = interval(avg, std, sigma=sigma)
+        ax.fill_between(nu, l, u, step='mid',
+                        color=p[0].get_color(), alpha=1/3, linewidth=0)
+
+    # x-axis must be in log scale; otherwise the bin boundaries are wrong
+    ax.set_xscale('log')
+    # optionally we may set y-axis to log sacle
+    if ylog:
+        ax.set_yscale('log')
+
+def step(ax, nu, avg, std=None, color=None, shade=None, label=None, **kwargs):
+    n   = len(nu)
+    avg = avg.reshape(n,-1)
+    for i in range(avg.shape[-1]):
+        stdi   = std.reshape(n,-1)[:,i] if std is not None else None
+        colori = color if color is not None else ('k'  if i == 0 else None)
+        widthi = 2 if i == 0 else 1
+        shadei = shade if shade is not None else (True if i == 0 else False)
+        labeli = label[i] if label is not None else None
+        step_one(ax, nu, avg[:,i], stdi,
+                 color=colori, linewidth=widthi,
+                 shade=shadei, label=labeli,
+                 **kwargs)
