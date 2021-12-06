b594ad099575c10a1b2fee3fe83d15ac93bf31ad
Ben Prather <bprathr2@illinois.edu>
2021-12-06 10:11:52 -0600

    Add reader for FITS files, from e.g. BHOSS runs by A. Cruz-Osorio

---
+++ common/io_fits.py
@@ -0,0 +1,83 @@
+# Copyright (C) 2021 Ben Prather
+# Copyright (C) 2020 Chi-kwan Chan
+# Copyright (C) 2020 Steward Observatory
+#
+# This file is part of `blackholepy`.
+#
+# `Blackholepy` is free software: you can redistribute it and/or
+# modify it under the terms of the GNU General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# `Blackholepy` is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with `blackholepy`.  If not, see <http://www.gnu.org/licenses/>.
+
+from astropy import units
+from astropy.io import fits
+
+from . import scale as s
+from . import dalt  as d
+
+def load_fits(f, pol=True, **kwargs):
+    """Load the contents of a fits file just like an ipole image.
+    Requires just the astropy.io.fits.hdu.image.PrimaryHDU object,
+    i.e. fits.open(fname)[0]
+    Note has to ASSUME mass and distance as they are not recorded
+    """
+
+    h = f.header
+    # These are unreliable/nonexistent in A.C.-O. FITS files
+    #MBH  = h['MBH'] * 1e6 * units.M_sun
+    #dist = h['dsource'] * units.cm
+    # This is needlessly precise so as to exactly
+    # match the effective value when converting
+    # ipole's L_unit in io_ipole.py
+    MBH = 4141166.64059526 * units.M_sun
+    L_unit = MBH.to(units.cm, equivalencies=s.GR).value
+    dsource = 2.5077305106e+22
+    dist = dsource * units.cm
+    freq = h['FREQ'] * units.Hz
+
+    time  = h['TIME']
+
+    nx = h['NAXIS1']
+    ny = h['NAXIS2']
+    # FOV/px ~ CDELT appears to be in degrees
+    fov_to_d = dsource / L_unit / (3600*1e6)
+    width = abs(nx*h['CDELT1']*fov_to_d)
+    height = abs(ny*h['CDELT2']*fov_to_d)
+
+    scale = (width * L_unit / nx) * (height * L_unit / ny) / (dsource * dsource) / 1e-23
+    img  =  f.data / scale
+
+    return d.Image(img, MBH, dist, freq, time, width, height, **kwargs)
+
+def load_img(f, **kwargs):
+    if isinstance(f, list):
+        return load_fits(f[0], **kwargs)
+    with fits.open(f) as g:
+        return load_fits(g[0], **kwargs)
+
+def load_summ(f, **kwargs):
+    raise NotImplementedError("Summary loading of FITS not implemented")
+
+def load_mov(fs, **kwargs):
+    if isinstance(fs, str):
+        fs = [fs]
+
+    times = []
+    imgs  = [] # collect arrays in list and then cast to np.array() in
+               # d.Image() all at once is faster than concatenate
+    for f in fs:
+        img = load_img(f, **kwargs)
+        times.append(img.meta.time)
+        imgs.append(img)
+
+    meta = img.meta
+    meta.time = units.Quantity(times)
+    return d.Image(imgs, meta=meta)
