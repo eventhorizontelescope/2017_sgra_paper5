0c04617de04f58cb16eebd3ee45326d43ed8cd89
Angelo Ricarte <aricarte@eht.astro.illinois.edu>
2022-10-14 12:40:29 -0500

    Added functions for polarization fractions

--- cache_summ.py
+++ cache_summ.py
@@ -86,20 +86,22 @@ def cache_summ(src_fmt, dst_fmt, img_fmt='ipole',
             Mdot, Ladv, nuLnu, Ftot, img = io.load_summ(p)
 
             moments = mm.moments(img.value, *img.fov.value, FWHM=True)
-            unresolvedPolarizationFractions = mm.unresolvedFractionalPolarizations(img.value)
+            unresolvedPolarizationFractions = mm.unresolvedFractionalPolarizations(img)
+            resolvedPolarizationFractions = mm.resolvedFractionalPolarizations(img)
             time    = img.meta.time.value
             time_hr = img.meta.time.to(u.hr).value
             tab.append([
                 time, time_hr,
                 Ladv, Mdot, nuLnu, Ftot, np.min(img.value), np.max(img.value),
-                *moments, *unresolvedPolarizationFractions])
+                *moments, *unresolvedPolarizationFractions, *resolvedPolarizationFractions])
 
         # Turn list of of list into pandas data frame
         tab = pd.DataFrame(tab, columns=[
             'time', 'time_hr',
             'Mdot', 'Ladv', 'nuLnu', 'Ftot',
             'Imin', 'Imax', 'Imean',
-            'alpha0', 'beta0', 'major_FWHM', 'minor_FWHM', 'PA', 'mnet', 'vnet']
+            'alpha0', 'beta0', 'major_FWHM', 'minor_FWHM', 'PA', 
+            'mnet', 'vnet', 'mavg', 'vavg']
         )
 
         # Only touch file system if everything works
--- common/analyses.py
+++ common/analyses.py
@@ -17,6 +17,8 @@
 # along with `blackholepy`.  If not, see <http://www.gnu.org/licenses/>.
 
 import numpy as np
+from .convolveSquareImage import *
+import pdb
 
 def moments(img, width, height, FWHM=False):
 
@@ -75,3 +77,14 @@ def unresolvedFractionalPolarizations(img):
         unresolvedLinear = np.sqrt(np.sum(img[:,:,1])**2 + np.sum(img[:,:,2])**2)
         unresolvedCircular = np.sum(img[:,:,3])
         return unresolvedLinear/totalFlux, unresolvedCircular/totalFlux
+
+def resolvedFractionalPolarizations(img, blurring_fwhm_muas=20.0):
+
+    if img.shape[2] <= 4:
+        #Return nan if there is no polarization data, which we check by just looking at the number of 2d arrays
+        return np.nan, np.nan
+    else:
+        assert np.isclose(np.abs(img.fov.value[0]), np.abs(img.fov.value[1]))
+        blurredStokesImages = [convolveSquareImage(img.value[:,:,stokes], np.abs(img.fov.value[0]), blurring_fwhm_muas) for stokes in range(4)]
+        resolvedLinear = np.sqrt(blurredStokesImages[1]**2 + blurredStokesImages[2]**2)
+        return np.nanmean(resolvedLinear/blurredStokesImages[0]), np.nanmean(blurredStokesImages[3]/blurredStokesImages[0])
