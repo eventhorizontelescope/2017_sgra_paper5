Fixed an off-by-one error with img shapes and tried to add functionality for fits

common/analyses.py
@@ -18,7 +18,6 @@
 
 import numpy as np
 from .convolveSquareImage import *
-import pdb
 
 def moments(img, width, height, FWHM=False):
 
@@ -68,7 +67,7 @@ def moments(img, width, height, FWHM=False):
 
 def unresolvedFractionalPolarizations(img):
 
-    if img.shape[2] <= 4:
+    if img.shape[2] < 4:
         #Return nan if there is no polarization data, which we check by just looking at the number of 2d arrays
         return np.nan, np.nan
 
@@ -80,7 +79,7 @@ def unresolvedFractionalPolarizations(img):
 
 def resolvedFractionalPolarizations(img, blurring_fwhm_muas=20.0):
 
-    if img.shape[2] <= 4:
+    if img.shape[2] < 4:
         #Return nan if there is no polarization data, which we check by just looking at the number of 2d arrays
         return np.nan, np.nan
 
@@ -92,7 +91,7 @@ def resolvedFractionalPolarizations(img, blurring_fwhm_muas=20.0):
 def computeBetaCoefficient(img, m=2, r_min=0, r_max=np.inf, norm_in_int=False, norm_with_StokesI=True):
     """Based on pmodes.py by Daniel Palumbo"""
 
-    if img.shape[2] <= 4:
+    if img.shape[2] < 3:
         #Return nan if there is no polarization data, which we check by just looking at the number of 2d arrays
         return np.nan, np.nan
 
@@ -161,7 +160,6 @@ def computeOpticalDepth(img):
     """Intensity-weighted average optical depth"""
 
     I = img.value[:,:,0]
-    pdb.set_trace()
     tau = img.tauI
     return np.sum(tau * I) / np.sum(I)
 

common/io_fits.py
@@ -57,14 +57,20 @@ def load_fits(f, pol=True, **kwargs):
     height = abs(ny*h['CDELT2']*3600*1e6*fov_to_d)
 
     scale = (width * L_unit / nx) * (height * L_unit / ny) / (dsource * dsource) / 1e-23
-    img  =  f.data.T / scale
+
+    if len(f.data.shape) == 2:
+        img  =  f.data.T / scale
+    elif len(f.data.shape) == 3:
+        img = np.transpose(f.data, (2,1,0)) / scale
+    tauI = None
+    tauF = None
 
     #print("L_unit: {}".format(L_unit))
     #print("FOV in deg: {} {} M: {} x {}".format(nx*h['CDELT1'], ny*h['CDELT2'], width, height))
     #print("Scale: {}".format(scale))
     #print("Total flux in image: {} header: {}".format(np.sum(img*scale), h['STOT']))
 
-    return d.Image(img, MBH, dist, freq, time, width, height, **kwargs)
+    return d.Image(img, MBH, dist, freq, time, width, height, tauI, tauF, **kwargs)
 
 def load_img(f, **kwargs):
     if isinstance(f, list):