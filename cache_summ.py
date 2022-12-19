Fixed bug with Ftot missing a factor of <scale>, which has now been added to the image meta.

cache_summ.py
@@ -32,8 +32,16 @@ from common import hallmark as hm
 from common import analyses as mm
 import pdb
 
-def cache_summ(src_fmt, dst_fmt, img_fmt='ipole',
-               params=None, order=['snapshot'], **kwargs):
+def cache_summ(src_fmt, dst_fmt, 
+               params=None, order=['snapshot'], FWHM=True, **kwargs):
+
+    file_ending = src_fmt.split('.')[-1]
+    if file_ending == 'fits':
+        img_fmt = 'fits'
+    elif file_ending == 'h5':
+        img_fmt = 'ipole'
+    else:
+        raise ValueError("You are asking for neither h5 nor fits files, which are not implemented.")
 
     io = import_module('common.io_' + img_fmt)
 
@@ -97,7 +105,7 @@ def cache_summ(src_fmt, dst_fmt, img_fmt='ipole',
         for p in tqdm(sel.path, desc=desc):
             Mdot, Ladv, nuLnu, Ftot, img = io.load_summ(p)
 
-            moments = mm.moments(img.value, *img.fov.value, FWHM=True)
+            moments = mm.moments(img.value, *img.fov.value, FWHM=FWHM)
             unresolvedPolarizationFractions = mm.unresolvedFractionalPolarizations(img)
             resolvedPolarizationFractions = mm.resolvedFractionalPolarizations(img)
             beta2Coefficient = mm.computeBetaCoefficient(img)
@@ -106,13 +114,13 @@ def cache_summ(src_fmt, dst_fmt, img_fmt='ipole',
             time    = img.meta.time.value
             time_hr = img.meta.time.to(u.hr).value
             tab.append([
-                time, time_hr,
+                p, time, time_hr,
                 Ladv, Mdot, nuLnu, Ftot, np.min(img.value), np.max(img.value),
                 *moments, *unresolvedPolarizationFractions, *resolvedPolarizationFractions, *beta2Coefficient, opticalDepth, faradayDepth])
 
         # Turn list of of list into pandas data frame
         tab = pd.DataFrame(tab, columns=[
-            'time', 'time_hr',
+            'file_path', 'time', 'time_hr',
             'Mdot', 'Ladv', 'nuLnu', 'Ftot',
             'Imin', 'Imax', 'Imean',
             'alpha0', 'beta0', 'major_FWHM', 'minor_FWHM', 'PA', 

common/io_fits.py
@@ -52,16 +52,13 @@ def load_fits(f, pol=True, **kwargs):
 
     nx = h['NAXIS1']
     ny = h['NAXIS2']
-    # FOV/px ~ CDELT appears to be in degrees
     fov_to_d = dsource / L_unit / 2.06265e11 # latter is muas_per_rad
     width = abs(nx*h['CDELT1']*3600*1e6*fov_to_d)
     height = abs(ny*h['CDELT2']*3600*1e6*fov_to_d)
-
     scale = (width * L_unit / nx) * (height * L_unit / ny) / (dsource * dsource) / 1e-23
 
-    #TODO:  Do these transposes make sense?
     if len(f.data.shape) == 2:
-        img  =  f.data.T / scale
+        img = f.data / scale
     elif len(f.data.shape) == 3:
         img = np.transpose(f.data[:5,:,:], (2,1,0)) / scale
         try:
@@ -73,12 +70,7 @@ def load_fits(f, pol=True, **kwargs):
         except IndexError:
             tauF = None
         
-    #print("L_unit: {}".format(L_unit))
-    #print("FOV in deg: {} {} M: {} x {}".format(nx*h['CDELT1'], ny*h['CDELT2'], width, height))
-    #print("Scale: {}".format(scale))
-    #print("Total flux in image: {} header: {}".format(np.sum(img*scale), h['STOT']))
-
-    return d.Image(img, MBH, dist, freq, time, width, height, tauI, tauF, **kwargs)
+    return d.Image(img, MBH, dist, freq, time, width, height, tauI, tauF, scale, **kwargs)
 
 def load_img(f, **kwargs):
     if isinstance(f, list):
@@ -103,7 +95,7 @@ def load_summ(f, **kwargs):
     """Most info will be missing.  Returning nan for those."""
     
     img = load_img(f, **kwargs)
-    Ftot = np.nansum(img[:,:,0])
+    Ftot = np.nansum(img[:,:,0]).value * img.scale
     Mdot = np.nan
     Ladv = np.nan
     nuLnu = np.nan